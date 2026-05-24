d684329b6a8e00f429e15d3e37eeac086f27aa06
Achyut Mahajan <achyutmahajan@users.noreply.github.com>
2026-05-24 14:16:17 +0530

    deployment setup

--- task-management-backend/server.js
+++ task-management-backend/server.js
@@ -18,7 +18,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+
+    origin: "*",
+
+    methods: [
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE"
+    ]
+}));
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 app.use("/api/projects", projectRoutes);
